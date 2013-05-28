436f76b057112ad4e5a91254db90fe3777e045ad
Darlan Alves <darlan@moovia.com>
2013-05-27 23:06:48 -0300

    NodeJS site setup

---
+++ web.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var fs = require('fs');
+var app = express();
+
+var readFile = function(file, response) {
+	console.log('Reading %s', file);
+	response.sendfile('./' + file, { maxAge: 3600*24*7*1000 });
+};
+
+//app.use(express.logger());
+app.use(express.compress());
+
+app.set('title', 'ClassJS');
+
+app.get('/', function(request, response) {
+	readFile('index.html', response);
+});
+
+app.get('/:dir/:file', function(request, response) {
+	readFile(request.params.dir + '/' + request.params.file, response);
+});
+
+var port = 80;
+app.listen(port);
+console.log('Server running on port %d', port);
